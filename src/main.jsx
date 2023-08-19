840e6789d52cb70ec8eecc217396354c4a6dc1f3
OltLatifi <oltlatifi2003@gmail.com>
2023-08-19 11:56:02 +0200

    changed the base url

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "./index.css"
 import axios from "axios"
-axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.baseURL = 'https://oltlatifi.pythonanywhere.com';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
